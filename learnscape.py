f6a201f9fb1a458cd2d4b8e3081600cce2c4d99f
Sean O'Beirne <sean.t.obeirne@gmail.com>
2024-09-23 12:49:29 -0400

    fixed up formatting on UI

--- learnscape.py
+++ learnscape.py
@@ -57,18 +57,28 @@ BR = '╯'
 # vertright = '├'
 # verthoriz = '┼'
 
-BORDER_COLOR = c.PURPLE
+BORDER_COLOR = c.DARK_GREY
 
 running = True
 
-wins = []
-CTRLPANEL_I = 0
-STATUSPANEL_I = 1
-TITLEPANEL_I = 2
-MENUPANEL_I = 3
-OPTIONSPANEL_I = 4
+wins = {}
 
 actions = {}
+
+MODES = {
+    1: "mainmenu",
+    2: "simulation",
+    3: "paused",
+}
+mode = "mainmenu"
+
+SIMULATIONS = {
+    1: "scheduler",
+    2: "memory management",
+    3: "deadlock",
+}
+
+simulation = ""
 
 
 
@@ -85,28 +95,35 @@ def draw():
 
 
 
-def draw_win(win: curses.window, borders=True, ascii=False, color=c.WHITE):
+def draw_win(win: curses.window, borders=True, ascii=False, color=c.WHITE, border_color=BORDER_COLOR):
     height, width = win.getmaxyx()
     h = height - 1
     w = width - 2
     if borders:
         for i in range(1, w):
-            win.addch(0, i, H, BORDER_COLOR)
-            win.addch(h, i, H, BORDER_COLOR)
+            win.addch(0, i, H, border_color)
+            win.addch(h, i, H, border_color)
         for i in range(1, h):
-            win.addch(i, 0, V, BORDER_COLOR)
-            win.addch(i, w, V, BORDER_COLOR)
-        win.addch(0, 0, TL, BORDER_COLOR)
-        win.addch(h, 0, BL, BORDER_COLOR)
-        win.addch(0, w, TR, BORDER_COLOR)
-        win.addch(h, w, BR, BORDER_COLOR)
+            win.addch(i, 0, V, border_color)
+            win.addch(i, w, V, border_color)
+        win.addch(0, 0, TL, border_color)
+        win.addch(h, 0, BL, border_color)
+        win.addch(0, w, TR, border_color)
+        win.addch(h, w, BR, border_color)
     if ascii:
         for i, line in enumerate(HEADER):
             win.addstr(i+1, 1, line, color)
 
 
 def draw_wins(height, width):
+    # clear first
+    for win in wins:
+        wins[win].erase()
+        wins[win].bkgd(' ', c.WHITE)
+        refresh()
+
     # bottom-left control panel
+    name = "ctrlpanel"
     ctrlpanel_height = 6
     ctrlpanel_width = 15
     ctrlpanel_y = height - ctrlpanel_height
@@ -122,9 +139,10 @@ def draw_wins(height, width):
     for i in range(len(ctrlpanel_text)):
         ctrlpanel.addstr(i+1, 2, ctrlpanel_text[i])
     draw_win(ctrlpanel)
-    wins.append(ctrlpanel)
+    wins[name] = ctrlpanel
 
     # bottom status bar
+    name = "statuspanel"
     statuspanel_height = 4
     statuspanel_width = width - ctrlpanel_width - 2
     statuspanel_y = height - statuspanel_height
@@ -132,52 +150,18 @@ def draw_wins(height, width):
     statuspanel = curses.newwin(statuspanel_height, statuspanel_width, statuspanel_y, statuspanel_x)
     statuspanel.bkgd(c.WHITE)
     statuspanel_text = [
-        "p - pause",
-        "r - reset",
     ]
+    statuspanel.addstr(2, 2, f"mode: {mode}")
     for i, line in enumerate(statuspanel_text):
         x = (statuspanel_width-1) // 2 - len(line) // 2
         statuspanel.addstr(i+1, x, line)
     draw_win(statuspanel)
-    wins.append(statuspanel)
+    wins[name] = statuspanel
 
-    # top left title
-    titlepanel_height = 7
-    titlepanel_width = 71
-    titlepanel_y = 0
-    titlepanel_x = 0
-    titlepanel = curses.newwin(titlepanel_height, titlepanel_width, titlepanel_y, titlepanel_x)
-    titlepanel.bkgd(c.WHITE)
-    draw_win(titlepanel, ascii=True, color=c.LIGHT_RED)
-    wins.append(titlepanel)
-
-    # center main menu
-    menupanel_height = 25
-    menupanel_width = 52
-    menupanel_y = 8
-    menupanel_x = width // 2 - menupanel_width // 2 - 20
-    menupanel = curses.newwin(menupanel_height, menupanel_width, menupanel_y, menupanel_x)
-    menupanel.bkgd(c.WHITE)
-    menupanel_text = [
-        "",
-        "Welcome!",
-        "Which concept would you like to visualize?",
-        "",
-        "1 - scheduler algorithms",
-        "2 - memory management",
-        "3 - deadlock",
-        # "4 - ",
-        # "5 - ",
-    ]
-    for i, line in enumerate(menupanel_text):
-        x = (menupanel_width-1) // 2 - len(line) // 2
-        menupanel.addstr(i+1, x, line, c.YELLOW if i < 3 else c.WHITE)
-    draw_win(menupanel)
-    wins.append(menupanel)
-
-    # right options maybe?
+    # right options
+    name = "optionspanel"
     optionspanel_height = 34
-    optionspanel_width = 40
+    optionspanel_width = 30
     optionspanel_y = 0
     optionspanel_x = width - optionspanel_width
     optionspanel = curses.newwin(optionspanel_height, optionspanel_width, optionspanel_y, optionspanel_x)
@@ -188,18 +172,79 @@ def draw_wins(height, width):
         # "? - help",
         # "q - quit"
     ]
+    optionspanel.addstr(1, 2, f"Options:", c.YELLOW)
     for i in range(len(optionspanel_text)):
         optionspanel.addstr(i+1, 2, optionspanel_text[i])
-    draw_win(optionspanel)
-    wins.append(optionspanel)
+    draw_win(optionspanel, border_color=c.DIM_WHITE)
+    wins[name] = optionspanel
+
+    # main centered panel for simulation
+    name = "mainpanel"
+    mainpanel_height = height - statuspanel_height - 4
+    mainpanel_width = statuspanel_width - optionspanel_width - 1
+    mainpanel_y = 4
+    mainpanel_x = ctrlpanel_width + 1
+    mainpanel = curses.newwin(mainpanel_height, mainpanel_width, mainpanel_y, mainpanel_x)
+    mainpanel.bkgd(c.WHITE)
+    mainpanel_text = [
+    ]
+    for i, line in enumerate(mainpanel_text):
+        x = (mainpanel_width-1) // 2 - len(line) // 2
+        mainpanel.addstr(i+1, x, line)
+    draw_win(mainpanel, border_color=c.DIM_WHITE)
+    wins[name] = mainpanel
+
+    # top left title
+    if mode == "mainmenu":
+        name = "titlepanel"
+        titlepanel_height = 7
+        titlepanel_width = 71
+        titlepanel_y = 0
+        titlepanel_x = 0
+        titlepanel = curses.newwin(titlepanel_height, titlepanel_width, titlepanel_y, titlepanel_x)
+        titlepanel.bkgd(c.WHITE)
+        draw_win(titlepanel, ascii=True, color=c.LIGHT_RED, border_color=c.PURPLE)
+        wins[name] = titlepanel
+
+    # center main menu
+    if mode == "mainmenu":
+        name = "menupanel"
+        menupanel_height = 25
+        menupanel_width = 52
+        menupanel_y = 8
+        menupanel_x = width // 2 - menupanel_width // 2 + statuspanel_x // 2
+        menupanel = curses.newwin(menupanel_height, menupanel_width, menupanel_y, menupanel_x)
+        menupanel.bkgd(c.WHITE)
+        menupanel_text = [
+            "",
+            "Welcome!",
+            "Which concept would you like to visualize?",
+            "",
+            "1 - scheduler algorithms",
+            "2 - memory management",
+            "3 - deadlock",
+            # "4 - ",
+            # "5 - ",
+        ]
+        for i, line in enumerate(menupanel_text):
+            x = (menupanel_width-1) // 2 - len(line) // 2
+            if len(line) > 0 and line[0].isdigit():
+                menupanel.addstr(i+1, x, line.split(" ")[0], c.LIGHT_GREEN)
+                x += 1
+                menupanel.addstr(i+1, x, line[1:], c.YELLOW if i < 3 else c.WHITE)
+            else:
+                menupanel.addstr(i+1, x, line, c.YELLOW if i < 3 else c.WHITE)
+        draw_win(menupanel, border_color=c.PURPLE)
+        wins[name] = menupanel
+
 
 def refresh():
     stdscr.refresh()
     for win in wins:
-        win.refresh()
+        wins[win].refresh()
 
 def update_status_last_key(key):
-    wins[STATUSPANEL_I].addstr(1, 2, f"last key: {key}")
+    wins["statuspanel"].addstr(1, 2, f"last key: {key}")
 
 
 def show_help(height, width):
@@ -210,7 +255,6 @@ def show_help(height, width):
     helppanel = curses.newwin(helppanel_height, helppanel_width, helppanel_y, helppanel_x)
     helppanel.bkgd(c.WHITE)
     helppanel_text = [
-
         "p - pause",
         "r - reset",
         "? - help", 
@@ -218,13 +262,14 @@ def show_help(height, width):
     ]
     for i in range(len(helppanel_text)):
         helppanel.addstr(i+1, 2, helppanel_text[i])
-    draw_win(helppanel)
+    draw_win(helppanel, border_color=c.LIGHT_RED)
     helppanel.refresh()
     key = stdscr.getkey()
     helppanel.erase()
     helppanel.bkgd(' ', c.WHITE)
     helppanel.refresh()
     del helppanel
+    draw_wins(height, width)
     return key
 
 
@@ -232,28 +277,40 @@ def main(stdscr):
     draw()
     height, width = stdscr.getmaxyx()
     x, y = 0, 0
+    global mode
+    mode = MODES[1]
+
     draw_wins(height, width)
+
 
     global running
     # Main loop
     while running:
         refresh()
-        key = stdscr.getkey()
-        if key == '?':
+        key: str = stdscr.getkey()
+        if key.isdigit():
+            mode = MODES[2]
+            simulation = SIMULATIONS[int(key)]
+            draw_wins(height, width)
+        elif key == '?':
             update_status_last_key(key)
             refresh()
             key = show_help(height, width)
         elif key == 'r':
             pass
         elif key == 'q' or key == '\x1b':
-            running = False
+            if mode == MODES[2]:
+                mode = MODES[1]
+                draw_wins(height, width)
+            else:
+                running = False
         elif key == ' ':
             refresh()
             continue
         else:
             log.error(f"Invalid key {key}")
         update_status_last_key(key)
-        log.info(f"keypress: {key}")
+        log.info(f"keypress: {key}, in mode: {mode}")
 
     log.info("Quitting...")
 
